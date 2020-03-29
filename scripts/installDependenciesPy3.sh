25b3e3b4c2054bdf36d5dbbadba6ab71d8779c22
tupq <pqtno30@gmail.com>
2020-03-29 18:39:17 +0700

    Update installDependenciesPy3.sh

--- scripts/installDependenciesPy3.sh
+++ scripts/installDependenciesPy3.sh
@@ -8,4 +8,6 @@ sudo apt-get install openjdk-8-jdk -y
 # Install other dependencies
 sudo apt-get install zip unzip autoconf automake libtool curl zlib1g-dev maven -y
 # Install Python 3.x
-sudo apt-get install python3-numpy swig python3-dev python3-pip python3-wheel -y
+sudo apt-get install python3-numpy swig python3-dev python3-pip python3-wheel python3-h5py -y 
+
+pip3 install keras_application keras_prepocessing
